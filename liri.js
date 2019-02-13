4cbfca56ca3ba7c5193261684f22de685168054c
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-02-13 10:03:33 -0700

    commit

--- liri.js
+++ liri.js
@@ -1,15 +1,15 @@
 // LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies
-require("dotenv").config(); // throws console error 
+require("dotenv").config();
 const keys = require("./keys");
 const Spotify = require("node-spotify-api");
-const spotify = new Spotify(keys.spotify); // throws console error (not defined)
+const spotify = new Spotify(keys.spotify);
 const omdb = require('omdb');
 const axios = require("axios");
 console.log(process.argv);
 
 
 // OMBD
-// var keys = require("./keys.js");
+
 const fs = require("fs");
 const request = require("request");
 const userInput = process.argv[2];
@@ -20,7 +20,6 @@ var userQuery = process.argv.join(" ");
 // Switches for commands
 function userCommand(userInput, userQuery) {
     switch (userInput) {
-        // able to fire URL request below but not with "movie-this" command
         case "movie-this":
             movieThis();
             break;
@@ -34,7 +33,7 @@ function userCommand(userInput, userQuery) {
             doThis(userQuery);
             break;
         default:
-            console.log("default");
+            console.log("Please enter one of the following commands after node liri.js followed by your query: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
             break;
     }
 }
@@ -49,19 +48,24 @@ function movieThis() {
     // Request is sent for info from the api and displayed to the console based on search parameters
     axios.get(queryUrl).then(
         function (response) {
-            console.log("Title: " + response.data.Title);
-            console.log("Year the movie came out: " + response.data.Year);
-            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
-            console.log("Origin Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
+            if (userQuery) {
+                console.log("Title: " + response.data.Title);
+                console.log("Year the movie came out: " + response.data.Year);
+                console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+                console.log("Origin Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            }
+            else {
+                console.log("If you haven't watched Mr. Nobody, then you should " + "http://www.imdb.com/title/tt0485947/");
+                console.log("It's on Netflix!");
+            }
         }
-    );
+    )
+
 };
-
-
 // BANDS IN TOWN
 
 // If "concert-this" function is called
@@ -78,7 +82,8 @@ function concertThis() {
             console.log(response.data[0].venue.name);
             // console.log("Name of the venue: " + response.data.Venue);
             console.log(response.data[0].venue.city + " " + response.data[0].venue.region);
-            console.log(response.data[1].datetime);
+            console.log(response.data[0].datetime);
+
         }
     );
 }
@@ -108,9 +113,9 @@ function spotifyThisSong(song) {
             console.log("Link: " + data.tracks.items[0].external_urls.spotify);
         } else {
             console.log("Preview: " + data.tracks.items[0].preview_url);
-        }
-        // album
-        console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+        } 
+
     });
 
 }
@@ -124,7 +129,7 @@ function doThis() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) { return console.log(error); }
         var args = data.split(", ");
-        
+
         userCommand(args[0], args[1]);
     });
 
